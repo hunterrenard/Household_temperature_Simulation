8dca67f66f91490da24128d508bbfcfd1adafe96
Hunter Renard <renardhj@gmail.com>
2018-02-19 17:52:32 -0500

    finished with code proto

--- hrenard_hvac.py
+++ hrenard_hvac.py
@@ -4,39 +4,39 @@ import math
 
 delta_t = 5 / 60 												# hr
 time_values = np.arange(0, 91 * 24, delta_t)
-change_over_time = time_values * 2 / 91
+change_over_time = time_values * 1.5 / 91
 
 # Outside Temperature
-a = 6 * np.sin(2 * math.pi * time_values) + 30
+a = 6 * np.sin(2 * math.pi * time_values) + 35
 b = np.random.normal(0, .1, len(time_values))
 
 outside_temperature = a + b.cumsum() + change_over_time
 
-# Thermostat - heat
-work = np.repeat(68, 9 / delta_t)
-home = np.repeat(74, 6 / delta_t)
-sleep = np.repeat(70, 9 / delta_t)
+# Thermostat - furnace
+work_furnace = np.repeat(68, 9 / delta_t)
+home_furnace = np.repeat(74, 6 / delta_t)
+sleep_furnace = np.repeat(70, 9 / delta_t)
 
-one_day = np.concatenate([work, home, sleep])
+one_day_furnace = np.concatenate([work_furnace, home_furnace, sleep_furnace])
 
-vacaction = np.repeat(55, 14 * 24 / delta_t)
-pre_vac = np.tile(one_day, 37)
-post_vac = np.tile(one_day, 40)
+vacaction_furnace = np.repeat(55, 14 * 24 / delta_t)
+pre_vac_furnace = np.tile(one_day_furnace, 37)
+post_vac_furnace = np.tile(one_day_furnace, 40)
 
-furnace = np.concatenate([pre_vac, vacaction, post_vac])
+furnace = np.concatenate([pre_vac_furnace, vacaction_furnace, post_vac_furnace])
 
 #Thermostat - A/C
-work2 = np.repeat(82, 9 / delta_t)
-home2 = np.repeat(79, 6 / delta_t)
-sleep2 = np.repeat(79, 9 / delta_t)
+work_AC = np.repeat(82, 9 / delta_t)
+home_AC = np.repeat(79, 6 / delta_t)
+sleep_AC = np.repeat(79, 9 / delta_t)
 
-one_day2 = np.concatenate([work2, home2, sleep2])
+one_day_AC = np.concatenate([work_AC, home_AC, sleep_AC])
 
-vacaction2 = np.repeat(88, 14 * 24 / delta_t)
-pre_vac2 = np.tile(one_day2, 37)
-post_vac2 = np.tile(one_day2, 40)
+vacaction_AC = np.repeat(88, 14 * 24 / delta_t)
+pre_vac_AC = np.tile(one_day_AC, 37)
+post_vac_AC = np.tile(one_day_AC, 40)
 
-AC = np.concatenate([pre_vac2, vacaction2, post_vac2])
+AC = np.concatenate([pre_vac_AC, vacaction_AC, post_vac_AC])
 
 # variables
 furnace_rate = 2											 	# degF / hr
@@ -55,8 +55,8 @@ inside_temperature = np.zeros(len(time_values))
 
 inside_temperature[0] = outside_temperature[0]								 	# degF
 
-furnace_cost_rate = 0											 	# (dollar / degF) / hr ???
-AC_cost_rate = 0											 	# (dollar / degF) / hr ???
+furnace_cost_rate = 5											 	# dollar / hour
+AC_cost_rate = 7											 	# dollar / hour
 
 #Loop
 for i in range(1, len(time_values)):
@@ -90,16 +90,13 @@ for i in range(1, len(time_values)):
 		else:
 			AC_on[i] = False						 			# boolean
 			AC_cool = 0								 		# degF / hr				
-	
-	# TODO: calculate costs from rate
-	furnace_cost = 0
-	AC_cost = 0
-
 	leakage_rate = (house_leakage_factor * (inside_temperature[i - 1] - outside_temperature[i - 1])) 	# degF / hr
 	T_prime = furnace_heat - AC_cool - leakage_rate
 	inside_temperature[i] = inside_temperature[i - 1] + T_prime * delta_t
 		
 # utility cost
+furnace_cost = furnace_on.sum() * (furnace_cost_rate * delta_t)								# dollar
+AC_cost = AC_on.sum() * (AC_cost_rate * delta_t)										# dollar
 print("estimated average monthly utility cost (in U.S. dollars): $" + str(furnace_cost + AC_cost))
 
 # Plotting
@@ -112,3 +109,4 @@ plt.plot(time_values, AC_on * 2 + 10, color='purple', linestyle="-", label="A/C 
 plt.ylim(0, 100)
 plt.legend()
 plt.show()
+
